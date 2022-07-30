a689e521420457d1550d37eb288d47357541c178
s-woon <sus9763@gmail.com>
2022-07-30 23:01:20 +0900

    서스펜스(스피너) 추가, 쿼리프로바이더 추가
    Co-authored-by: jong6598 <jong6598@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { BrowserRouter } from "react-router-dom";
+import Spinner from "./components/Spinner";
+
+const quertClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Suspense fallback={<Spinner />}>
+    <QueryClientProvider client={quertClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
