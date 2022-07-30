890c16061a766dbe8ba2710666fbaf03d31fe7e3
s-woon <sus9763@gmail.com>
2022-07-30 23:00:16 +0900

    Chat 페이지 생성 및 라우팅
    Co-authored-by: jong6598 <jong6598@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 import Intro from "./pages/Intro";
 import Home from "./pages/Home";
@@ -9,10 +10,12 @@ import Mypage from "./pages/Mypage";
 import Post from "./pages/Post";
 import PostDetail from "./pages/PostDetail";
 import PostEdit from "./pages/PostEdit";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
     <div className="App">
+      {/* <GlobalStyle /> */}
       <Routes>
         <Route path="/" element={<Intro />} />
         <Route path="/home" element={<Home />} />
@@ -22,6 +25,7 @@ function App() {
         <Route path="/post" element={<Post />} />
         <Route path="/post/:postId" element={<PostDetail />} />
         <Route path="/postedit" element={<PostEdit />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </div>
   );
