7bb321243c703d585223a94f5929867799a4f79a
s-woon <sus9763@gmail.com>
2022-08-01 23:08:10 +0900

    서버연결
    Co-authored-by: jong6598 <jong6598@users.noreply.github.com>

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: "http://43.200.6.110",
+  // baseURL: "https://a99b-49-143-101-137.jp.ngrok.io",
+  headers: {
+    'content-type': 'application/json;charset=UTF-8',
+    accept: 'application/json,',
+  },
 });
+
+instance.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('TOKEN');
+  config.headers.common['Authorization'] = `${accessToken}`;
+  return config;
+})
 
 export default instance;
