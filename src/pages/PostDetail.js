a4d6e43e63888c14a1ac0fafe9e0eaf285ac0ea3
s-woon <sus9763@gmail.com>
2022-08-02 21:19:25 +0900

    게시글 상세페이지 CSS 수정
    Co-authored-by: jong6598 <jong6598@users.noreply.github.com>

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AiOutlineHome } from "react-icons/ai";
 import Footer from "../components/Footer";
+import { FaCarrot } from "react-icons/fa";
 
 const PostDetail = () => {
   const navigate = useNavigate();
@@ -77,24 +78,29 @@ const PostDetail = () => {
         <AiOutlineHome />
       </HomeBtn>
       {postInfo.nickname === nickname ? (
-        <>
-          <Ubtn onClick={() => navigate("/post")}>수정</Ubtn>
+        <Btnbox>
+          <Ubtn onClick={() => navigate(`/post/${params.postId}/edit`)}>수정</Ubtn>
           <Dbtn onClick={contentDeleteBtn}>삭제</Dbtn>
-        </>
+        </Btnbox>
       ) : null}
       <img src={postInfo.imageUrl} alt="" />
-      <Userbox>
-        <span>작성자:{postInfo.nickname}</span>
-        <span>위치:{postInfo.area}</span>
-      </Userbox>
+      <ProfileBox>
+      <FaCarrot />
+        <NicknameBox>
+          <span>작성자:{postInfo.nickname}</span>
+          <span>위치:{postInfo.area}</span>
+          </NicknameBox>
+          <HeartBtn onClick={toggleLike}> {postInfo.isLiked===true?(<img src="/image/heart.png" alt="heartlogo"/>):(<img src="/image/emptyheart.png" alt="emptyheartlogo"/>)} </HeartBtn>
+      </ProfileBox>
       <Postbox>
         <span>{postInfo.title}</span>
-        <span>{postInfo.category}</span>
-        <span>{postInfo.after}</span>
-        <span>{postInfo.price}</span>
-        <span>{postInfo.content}</span>
+        <Category>
+        <span>카테고리: {postInfo.category} /</span>
+        <span>작성시간: {postInfo.after}</span>
+        </Category>
+        <span>가격: {postInfo.price}</span>
+        <span>내용: {postInfo.content}</span>
       </Postbox>
-      <HeartBtn onClick={toggleLike}> {postInfo.isLiked===true?(<img src="/image/heart.png" alt="heartlogo"/>):(<img src="/image/emptyheart.png" alt="emptyheartlogo"/>)} </HeartBtn>
       <Footer />
     </MainDiv>
   );
@@ -103,12 +109,13 @@ const PostDetail = () => {
 export default PostDetail;
 
 const MainDiv = styled.div`
+  position: relative;
   width: 40rem;
   height: 100vh;
-  margin: 0 auto;
+  margin: 0 auto 7rem;
   justify-content: center;
   background: #fffbf7;
-
+  /* background: #333; */
   img {
     display: block;
     margin: 1.3rem auto 0;
@@ -119,52 +126,108 @@ const MainDiv = styled.div`
 
 const HomeBtn = styled.button`
   font-size: 3rem;
-  margin-left: -25rem;
+  margin-left: 5rem;
+  display: flex;
+  text-align: left;
   margin-top: 1rem;
   background-color: transparent;
   border: 0;
+  color:#e78111;
 `;
+
+const Btnbox = styled.div`
+  background: #fffbf7;
+  display: flex;
+  padding-left: 27rem;
+  flex-direction: row;
+  `
+
 
 const Ubtn = styled.button`
   display: flex;
-  float: right;
-  margin: 1rem 5rem 1rem 2rem;
-  font-size: 2rem;
+  text-align: right;
+  font-size: 1.8rem;
+  background-color: #e78111;
+  border-radius: 0.5rem;
+  border-color: #e78111;
+  border-width: 0.2rem;
+  color: white;
 `;
 const Dbtn = styled.button`
   display: flex;
-  float: right;
-  font-size: 2rem;
-  margin-top: 1rem;
-  margin-left: 2rem;
+  text-align: right;
+  font-size: 1.8rem;
+  margin-left: 1rem;
+  background-color: #e78111;
+  border-radius: 0.5rem;
+  border-color: #e78111;
+  border-width: 0.2rem;
+  color: white;
 `;
 
-const Userbox = styled.div`
-  background: #ffecd7;
-  height: 10%;
-  display: flex;
-  flex-direction: column;
-  margin: 1.3rem auto 0;
-  max-width: 30rem;
-  text-align: center;
 
-  img {
-    width: 4rem;
-    height: 4rem;
+const ProfileBox = styled.div`
+  width: calc(100% - 6rem);
+  margin: 2rem auto 0;
+  cursor: pointer;
+  height: 7rem;
+  display: flex;
+  flex-direction: row;
+  text-align: left;
+  svg {
+    width: 5rem;
+    height: 5rem;
+    color: #e78111;
+    padding: 0.3rem;
+    margin: 0 1rem;
+    border-radius: 5rem;
+    border: 2px solid #e78111;
   }
 `;
+
+const NicknameBox = styled.div`
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  padding: 1rem 1rem;
+  margin-right: 1rem;
+  span {
+    margin: 0.2rem;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+`;
+
 const Postbox = styled.div`
   background: #ffecd7;
   display: flex;
   flex-direction: column;
   margin: 1.3rem auto 0;
   max-width: 30rem;
-  text-align: center;
+  text-align: left;
+  span{
+    margin: 0.7rem;
+    font-size: 1.2rem;
+    font-weight: 350;
+  }
 `;
 
 const HeartBtn = styled.button`
   max-width:3rem;
+  margin-left: 8rem;
   border: 0;
   background-color: transparent;
   max-height: 6rem;
+`
+
+const Category = styled.div`
+  display: flex;
+  flex-direction: row;
+  span {
+    margin: 0.3rem;
+    font-size: 1rem;
+    color: gray;
+    font-weight: 500;
+    padding-left: 0.5rem;
+  }
 `
