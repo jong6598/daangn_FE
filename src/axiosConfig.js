3569b05f26596deb1ec6c8b319283c641142749c
s-woon <sus9763@gmail.com>
2022-08-04 13:41:15 +0900

    테스트용 url 삭제
    Co-authored-by: jong6598 <jong6598@users.noreply.github.com>

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://43.200.6.110",
-  // baseURL: "http://localhost:5001",
-  // baseURL: "http://spartawoong.shop",
+
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     accept: 'application/json,',
